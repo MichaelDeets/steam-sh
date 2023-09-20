b82353f5595e0845cd77640ef0bb5b7ae92f8380
deets <MichaelDeets@users.noreply.github.com>
2023-09-20 21:09:38 +0100

    Update cs2.sh
    Updated formatting

--- cs2.sh
+++ cs2.sh
@@ -1,11 +1,8 @@
 #!/bin/sh -a
 
-# This script was specifically designed to be ran on my computer alone.	
-# You should not use this script directly, but rather as a base for a new script.
-
-# WINESYNC requires the winesync kernel module. If you do not have this kernel
-# module installed, it will not work. You should instead enable WINEESYNC or WINE
-# FSYNC, and disable WINESYNC.
+# This script was specifically designed to be ran on my computer alone.	 You should not use this script directly, but rather as a base for a new script.
+# WINESYNC requires the winesync kernel module. If you do not have this kernel module installed, it will not work. You should instead enable WINEESYNC
+# or WINE FSYNC, and disable WINESYNC.
 
 WINEARCH="win64"
 WINEDEBUG="-all"
@@ -13,12 +10,10 @@ WINEESYNC=0
 WINEFSYNC=0
 WINESYNC=1
 
-# WINE_ROOT should point to your WINE installation main folder, as it is used to
-# define WINE's location. This makes it easy to use pre-built WINE forks such as
-# WINE-GE. In this case, I am using the location defined by eselect in Gentoo
-# where it is pointing to my compiled version of GE's fork of Valve's Proton WINE.
-# We are setting up the location inside ~/Games/steam-wine, feel free to change
-# the location by changing the BASE_INSTALL location
+# WINE_ROOT should point to your WINE installation main folder, as it is used to define WINE's location. This makes it easy to use pre-built WINE forks such
+# as WINE-GE. In this case, I am using the location defined by eselect in Gentoo where it is pointing to my compiled version of GE's fork of Valve's Proton WINE.
+
+# We are setting up the location inside ~/Games/steam-wine, feel free to change the location by changing the BASE_INSTALL location
 
 BASE_INSTALL="~/Games/steam-wine"
 
@@ -28,21 +23,17 @@ WINE=$WINE_ROOT/bin/wine
 WINESERVER=$WINE_ROOT/bin/wineserver
 WINEPREFIX=$BASE_INSTALL/wine/prefix
 
-# For this install, we have defined a location for Steam to be installed.
-# This location MUST be selected when installing Steam from SteamSetup.exe.
-# By default, Steam will be installed within the WINE prefix, which can be 
-# extremely annoying if the prefix gets deleted.
+# For this install, we have defined a location for Steam to be installed. This location MUST be selected when installing Steam from SteamSetup.exe.
+# By default, Steam will be installed within the WINE prefix, which can be extremely annoying if the prefix gets deleted.
 
 STEAM_ROOT=$BASE_INSTALL/steamroot/
 STEAM_EXE=$STEAM_ROOT/steam.exe
 STEAM_ARGS="-nointro"
 DXVK_CONFIG_FILE="~/.config/dxvk/dxvk.conf"
 
-# I previously had issues were MESA would not render smokes, with or without
-# vulkan; these issues were due to compiling MESA with -Ofast, more specifically
+# I previously had issues were MESA would not render smokes, with or without vulkan; these issues were due to compiling MESA with -Ofast, more specifically
 # one of the flags -Ofast enables.
-# Only attempt switching drivers if you find problems using MESA, otherwise you
-# should always be using MESA.
+# Only attempt switching drivers if you find problems using MESA, otherwise you should always be using MESA.
 
 # For AMDGPU-PRO you should define the ICD filename locations
 #VK_ICD_FILENAMES="/usr/share/vulkan/icd.d/amd_pro_icd64.json"
@@ -50,12 +41,9 @@ DXVK_CONFIG_FILE="~/.config/dxvk/dxvk.conf"
 # For AMDVLK, instead use the envvar:
 #AMD_VULKAN_ICD=AMDVLK
 
-# Rather than using the default WINE prefix location (~/.wine), we will have the
-# prefix installed locally at the location defined before.
-# Probably don't need to include the WINEPREFIX/WINE envvars, but just to make
-# sure they're actually being used.
-# These winetricks variables were taken from the Lutris Steam install script, so I
-# would assume they are correct.
+# Rather than using the default WINE prefix location (~/.wine), we will have the prefix installed locally at the location defined before.
+# Probably don't need to include the WINEPREFIX/WINE envvars, but just to make sure they're actually being used.
+# These winetricks variables were taken from the Lutris Steam install script, so I would assume they are correct.
 
 echo "--- Creating WINE prefix ---"
 if [ ! -d $WINEPREFIX ]; then
@@ -64,10 +52,11 @@ if [ ! -d $WINEPREFIX ]; then
 	WINEPREFIX=${WINEPREFIX} winetricks msls31 riched20 andale arial comicsans impact tahoma times allfonts d3dcompiler_43 d3dcompiler_47 fontsmooth=rgb 
 fi
 
-# If you have a DXVK installation from your package manager, this will install
-# the DXVK binaries inside the WINE prefix. You might need to change the location
-# of setup_dxvk.sh, if it does not exist, you can download it from here:
+# If you have a DXVK installation from your package manager, this will install the DXVK binaries inside the WINE prefix.
+# You might need to change the location of setup_dxvk.sh, if it does not exist, you can download it from here:
+
 # https://raw.githubusercontent.com/doitsujin/dxvk/4f90d7bf5f9ad785660507e0cb459a14dab5ac75/setup_dxvk.sh
+
 # and change the script accordingly, otherwise just comment this out.
 
 echo "--- Installing DXVK ---"
@@ -78,9 +67,8 @@ fi
 # Taken directly from csgo.sh.
 # I like to include multiple .sh scripts containing various settings and envvars.
 # One before the while loop, and one after.
-#
-# You might need to use either "source" or simply a "." before the script
-# location to actually load. I have success using the latter. 
+
+# You might need to use either "source" or simply a "." before the script location for it to actually load. I have success using the latter. 
 
 echo "--- Launching Steam ---"
 STATUS=42
